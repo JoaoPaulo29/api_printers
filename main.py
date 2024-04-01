c1590ee7fa3499791d0d78e78094b41ab514fc08
JoaoPaulo29 <joaopaulomanuel71@gmail.com>
2024-04-01 16:46:35 +0100

    Update the methods

--- main.py
+++ main.py
@@ -15,28 +15,26 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/get/printers', methods=['GET'])
 def get_printers():
     if conn :
-        return jsonify(conn.getPrinters())
+        return jsonify({'printers':conn.getPrinters(), 'status': 200})
     else:
-        return "Erro ao ligar a CUPS!"
+        return jsonify({'error':"Erro ao ligar a CUPS!", 'status':500})
 
 @app.route('/print', methods=['POST'])
 def print_pdf():
-    data = request.json
-    printer_name = data.get('printer')
-    file_name = data.get(f'file_name')
-    settings = data.get('settings')
-    #return jsonify(data)
-    if not printer_name or not file_name :
-        return jsonify({'error': 'O nome da impressoara ou o arquivo são necessarios!'})
-    try:
-        file = request.files['file']
-        file_path = os.path.join(app.config['UPLOAD_FOLDER', file_name])
-        file.save()
-        conn.printFile(printer_name, file_path, file_name, settings)
-        os.remove(file_path)
-        return jsonify({'message': f'Arquivo {file_name} impresso com sucesso na impressora {printer_name}!'})
+    if 'file' not in request.files :
+        return jsonify({'error': 'No file part', 'status':400})
+    file = request.files['file']
+    printer_name = request.form['printer']
+    
+    if not printer_name or not file.filename :
+        return jsonify({'error': 'O nome da impressoara ou o arquivo são necessarios!', 'status':400})
+    try: 
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(file_path)
+        conn.printFile(printer_name, file_path, file.filename, {}) 
+        return jsonify({'message': f'Arquivo {file.filename} impresso com sucesso na impressora {printer_name}!', 'status':200})
     except Exception as e:
-        return jsonify({'error': f'Erro ao imprimir o arquivo {file_name}  : {e}'})
+        return jsonify({'error': f'Erro ao imprimir o arquivo {file.filename}  : {e}', 'status': 500})
 
  
 @app.route('/')
